82c62a559dd63c3028f8ab1d4ce4bdc803d0ad18
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-01-30 02:15:20 +0900

    😺 :: 혼공파 3주차 미션 완료

---
+++ ch03/Q01.py
@@ -0,0 +1,7 @@
+# list
+list_a = [274, 32, "문자열", False]
+print(list_a[1]) #32
+print(list_a[-1]) #False
+list_a[1] = "바껴라"
+print(list_a[1]) #바껴라
+print(list_a) #[274, 바껴라, "문자열", False]
---
+++ ch03/Q03.py
@@ -0,0 +1,12 @@
+# 범위 
+
+# while (continue)
+
+
+numbers = [5,15,6,20,7,25]
+
+for number in numbers:
+    if number < 10:
+        continue
+
+    print(number)
